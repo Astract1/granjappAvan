1c9b9f3e6f798cdcad6b1f3db4f79354cb900e35
Astract <diegojimenez1924@gmail.com>
2024-05-06 22:53:03 -0500

    Fix

--- app/src/main/java/com/example/granjapp/IniciarSesionCompradorActivity.java
+++ app/src/main/java/com/example/granjapp/IniciarSesionCompradorActivity.java
@@ -2,9 +2,6 @@ package com.example.granjapp;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
