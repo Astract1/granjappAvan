9066621408a4936c61c1117e7b44b93d449ab6a4
Astract <diegojimenez1924@gmail.com>
2024-05-06 22:52:31 -0500

    Fix

--- app/src/main/java/com/example/granjapp/RegistroGranjeroActivity.java
+++ app/src/main/java/com/example/granjapp/RegistroGranjeroActivity.java
@@ -1,5 +1,6 @@
 package com.example.granjapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -74,7 +75,7 @@ public class RegistroGranjeroActivity extends AppCompatActivity {
         long resultado = dbHelper.insertarCampesino(nombre, apellido, correo, contrasena, nombreGranja);
         if (resultado != -1) {
             Toast.makeText(this, "Campesino registrado correctamente", Toast.LENGTH_SHORT).show();
-            finish(); // Finalizar la actividad actual
+            Intent intent = new Intent(this, IniciodeAppActivity.class);
         } else {
             Toast.makeText(this, "Error al registrar el campesino", Toast.LENGTH_SHORT).show();
         }
